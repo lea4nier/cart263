7c6859c9703c99c1d9e829cb798048b927a35f67
Léa Fournier <lea.fournier@gmail.com>
2024-02-01 15:17:52 -0500

    testing speech recognition

--- projects/voices-jam/js/script.js
+++ projects/voices-jam/js/script.js
@@ -7,7 +7,7 @@ author, and this description to match your project!
 */
 
 "use strict";
-
+let speechRecognizer = new p5.SpeechRec();
 
 /**
 Description of preload
@@ -21,7 +21,10 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+    // 2. Tell the program the function to call if the recognizer hears something
+    speechRecognizer.onResult = handleResult;
+    // 3. Tell the recognizer to start listening
+    speechRecognizer.start();
 }
 
 
@@ -30,4 +33,13 @@ Description of draw()
 */
 function draw() {
 
+}
+
+function handleResult() {
+    // 4. Check if there is definitely a result
+    if (speechRecognizer.resultValue === true) {
+        // 5. If there is, print it out in the console
+        console.log(speechRecognizer.resultString);
+        // (e.g. speechRecognizer.resultString has what was said inside it)
+    }
 }
