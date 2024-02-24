e32afb021885c9defa5455008e5aa252d167ab9c
Léa Fournier <lea.fournier@gmail.com>
2024-02-24 10:00:30 -0500

    added serial port library, running tests with example code

--- projects/ai-jam/js/script.js
+++ projects/ai-jam/js/script.js
@@ -1,33 +1,44 @@
-/**
-Title of Project
-Author Name
+let handpose;
+let video;
+let hands = [];
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-"use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.size(width, height);
 
+    handpose = ml5.handpose(video, modelReady);
+
+    // This sets up an event that fills the global variable "predictions"
+    // with an array every time new hand poses are detected
+    handpose.on("hand", results => {
+        hands = results;
+    });
+
+    // Hide the video element, and just show the canvas
+    video.hide();
 }
 
+function modelReady() {
+    console.log("Model ready!");
+}
 
-/**
-Description of draw()
-*/
 function draw() {
+    image(video, 0, 0, width, height);
 
+    // We can call both functions to draw all keypoints and the skeletons
+    drawKeypoints();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+    for (let i = 0; i < hands.length; i += 1) {
+        const hand = hands[i];
+        for (let j = 0; j < hand.landmarks.length; j += 1) {
+            const keypoint = hand.landmarks[j];
+            fill(0, 255, 0);
+            noStroke();
+            ellipse(keypoint[0], keypoint[1], 10, 10);
+        }
+    }
 }
