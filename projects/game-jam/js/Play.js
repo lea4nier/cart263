9f2e3303e88e2151b08bb55be64a56b7c1f9354e
Léa Fournier <lea.fournier@gmail.com>
2024-03-23 13:24:09 -0400

    added avatar animation!

--- projects/game-jam/js/Play.js
+++ projects/game-jam/js/Play.js
@@ -4,13 +4,46 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        // create background
         this.background = this.add.sprite(0, 0, 'background');
-        this.background.setOrigin(0, 0); // set the origin to the top-left corner
-        this.background.setScale(800 / this.background.width, 600 / this.background.height); // scale 
+        this.background.setOrigin(0, 0);
+        this.background.setScale(800 / this.background.width, 600 / this.background.height);
 
+        // create avatar sprite
+        this.avatar = this.physics.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'avatar');
+        this.avatar.setDisplaySize(50, 50);
+
+        // create animations
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('avatar', { start: 0, end: 1 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        // Set up keyboard input
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
+        // reset avatar velocity
+        this.avatar.setVelocity(0);
 
+        // avatar movement and animation with arrow keys
+        if (this.cursors.left.isDown) {
+            this.avatar.setVelocityX(-100);
+            this.avatar.anims.play('walk', true);
+        } else if (this.cursors.right.isDown) {
+            this.avatar.setVelocityX(100);
+            this.avatar.anims.play('walk', true);
+        } else if (this.cursors.up.isDown) {
+            this.avatar.setVelocityY(-100);
+            this.avatar.anims.play('walk', true);
+        } else if (this.cursors.down.isDown) {
+            this.avatar.setVelocityY(100);
+            this.avatar.anims.play('walk', true);
+        } else {
+            this.avatar.anims.stop('walk');
+        }
     }
 }
--- projects/game-jam/js/boot.js
+++ projects/game-jam/js/boot.js
@@ -5,6 +5,11 @@ class Boot extends Phaser.Scene {
 
     preload() {
         this.load.image('background', 'assets/images/room.png');
+        this.load.spritesheet('avatar', 'assets/images/avatar.png', {
+            frameWidth: 16,
+            frameHeight: 16,
+            endFrame: 1
+        });
         this.load.on('complete', () => {
             this.scene.start('play');
         });
