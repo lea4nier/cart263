0111057909897d945afda096bd2ba618aa626628
Léa Fournier <lea.fournier@gmail.com>
2024-03-23 16:01:27 -0400

    fixing bugs with boot scene

--- projects/game-jam/js/main.js
+++ projects/game-jam/js/main.js
@@ -1,15 +1,26 @@
+/***********************
+Don't get possessed!
+Léa Fournier
+
+Turn the lights on and off to avoid the ghost. 
+***********************/
 "use strict";
 
 let config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    type: Phaser.AUTO, // set the renderer type to automatically choose between WebGL and Canvas.
+    width: 800, // set the width of the game canvas to 800 pixels.
+    height: 600, // set the height of the game canvas to 600 pixels.
     physics: {
-        default: 'arcade',
+        default: 'arcade', // set the default physics system to Arcade Physics.
     },
-    scene: [Boot, Play],
+    scene: [Boot, Play], // specify the scenes to be used in the game (Boot and Play)
 
-    gravity: { y: 200 }
+    gravity: { y: 200 } //set the gravity of the game world along the y-axis to 200 units.
 };
 
-let game = new Phaser.Game(config);
+let game = new Phaser.Game(config); // create a new game
+
+
+
+
+
