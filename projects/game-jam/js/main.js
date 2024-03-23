3da17c61206b21b094277d00806b4d0c12e572f9
Léa Fournier <lea.fournier@gmail.com>
2024-03-23 09:35:02 -0400

    testing that phaser works from tutorial code on their website

--- projects/game-jam/js/main.js
+++ projects/game-jam/js/main.js
@@ -1,33 +1,42 @@
-/**
-Title of Project
-Author Name
+class Example extends Phaser.Scene {
+    preload() {
+        this.load.setBaseURL('https://labs.phaser.io');
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+        this.load.image('sky', 'assets/skies/space3.png');
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        this.load.image('red', 'assets/particles/red.png');
+    }
 
-"use strict";
+    create() {
+        this.add.image(400, 300, 'sky');
 
+        const particles = this.add.particles(0, 0, 'red', {
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD'
+        });
 
-/**
-Description of preload
-*/
-function preload() {
+        const logo = this.physics.add.image(400, 100, 'logo');
 
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        particles.startFollow(logo);
+    }
 }
 
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    }
+};
 
-/**
-Description of setup
-*/
-function setup() {
-
-}
-
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+const game = new Phaser.Game(config);
