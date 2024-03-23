2e8048517f6bb5ef487cfccc7acaf0c019f7f70b
Léa Fournier <lea.fournier@gmail.com>
2024-03-23 10:04:48 -0400

    adding Play.js

---
+++ projects/game-jam/js/boot.js
@@ -0,0 +1,19 @@
+class Boot extends Phaser.Scene {
+    constructor() {
+        super({ key: 'boot' });
+    }
+
+    preload() {
+        this.load.on('complete', () => {
+            this.scene.start('play');
+        });
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
