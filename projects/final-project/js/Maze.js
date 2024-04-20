a32802f786ee293b3d443ea0a22d8af35bc20936
Léa Fournier <lea.fournier@gmail.com>
2024-04-20 15:40:09 -0400

    added key image for girl to collect

--- projects/final-project/js/Maze.js
+++ projects/final-project/js/Maze.js
@@ -4,16 +4,20 @@ class Maze extends Phaser.Scene {
     }
 
     create() {
-        // Add the evil stepmother to the scene
+        // add the evil stepmother to the scene
         this.mother = this.physics.add.sprite(100, 100, 'mother');
-        this.mother.setDisplaySize(80, 80);
+        this.mother.setDisplaySize(80, 80); //resize sprite
 
-        // Add the girl avatar to the scene
+        // add the girl avatar to the scene
         this.ella = this.physics.add.sprite(700, 500, 'ella');
-        this.ella.setDisplaySize(80, 80);
-        // Set up keyboard input for controlling the girl avatar (if needed)
+        this.ella.setDisplaySize(80, 80); //resize sprite
+
+        // set up keyboard input for controlling the girl avatar (if needed)
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        //add key image 
+        this.key = this.physics.add.sprite(700, 100, 'key');
+        this.key.setDisplaySize(30, 50); //resize image
     }
 
     update() {
