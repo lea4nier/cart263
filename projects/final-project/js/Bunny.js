ab4e42e8c340d9ec8981c5fd7b13a0885efec43f
Léa Fournier <lea.fournier@gmail.com>
2024-04-21 08:21:52 -0400

    images of bunny and 2 pills appear

--- projects/final-project/js/Bunny.js
+++ projects/final-project/js/Bunny.js
@@ -9,25 +9,14 @@ class Bunny extends Phaser.Scene {
         this.background.setOrigin(0, 0); // set the origin of the background 
         this.background.setScale(800 / this.background.width, 600 / this.background.height); // scale the background 
 
-        // // add the girl avatar to the scene
-        // this.ella = this.physics.add.sprite(300, 450, 'ella');
-        // this.ella.setDisplaySize(80, 80); //resize sprite
+        // add the red pill image
+        this.red = this.physics.add.sprite(300, 500, 'red');
+        this.red.setDisplaySize(80, 80); //resize sprite
 
-        // // create dragon sprite
-        // this.fire = this.physics.add.sprite(500, 400, 'fire'); // create dragon sprite with physics
-        // this.fire.setDisplaySize(315, 192); // set the display size of the dragon sprite
+        // add the blue pill image
+        this.blue = this.physics.add.sprite(500, 500, 'blue'); // create dragon sprite with physics
+        this.blue.setDisplaySize(80, 80); // set the display size of the dragon sprite
 
-        // // create animation for dragon breathing fire sprite
-        // this.anims.create({
-        //     key: 'fire',
-        //     frames: this.anims.generateFrameNumbers('fire', { start: 0, end: 23 }), //only 4 frames
-        //     frameRate: 10,
-        //     repeat: 0
-        // });
-        // this.fire.anims.play('fire', true); //play animation
-
-        // //create new instance of action menu
-        // this.actionMenu = new ActionMenu(this, 230, 350, ['Fight', 'Laugh']); //2 buttons, fight or laugh
     }
 
     update() {
